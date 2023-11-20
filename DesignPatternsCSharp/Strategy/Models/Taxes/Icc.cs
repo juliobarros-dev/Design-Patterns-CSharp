4e6d9c69152e0c7f0197859a7e6091e8fa0ac13e
Julio Barros <juliobarros.dev@gmail.com>
2023-11-20 20:16:57 -0300

    Adding new tax with a more complex rule without changing the calculator

---
+++ DesignPatternsCSharp/Strategy/Models/Taxes/Icc.cs
@@ -0,0 +1,19 @@
+﻿using Strategy.Abstractions;
+
+namespace Strategy.Models.Taxes
+{
+    internal class Icc : ITax
+    {
+        public double Calculate(Budget budget)
+        {
+            // Now we created a new tax with a more complexity rule to calculate it's value without the need to change anything on our calculator
+
+            return budget.Value switch
+            {
+                < 1000 => budget.Value * 0.05,
+                < 3000 => budget.Value * 0.07,
+                _ => (budget.Value * 0.08) + 30,
+            };
+        }
+    }
+}
--- DesignPatternsCSharp/Strategy/Program.cs
+++ DesignPatternsCSharp/Strategy/Program.cs
@@ -12,5 +12,16 @@ Budget budget = new(500.00);
 
 taxCalculator.Calculate(budget, icms);
 taxCalculator.Calculate(budget, iss);
+ITax icc = new Icc();
 
-// After executing the program the result must be: 50 and 30
+Budget budget1 = new(500);
+Budget budget2 = new(2000);
+Budget budget3 = new(5000);
+
+taxCalculator.Calculate(budget1, icms);
+taxCalculator.Calculate(budget1, iss);
+taxCalculator.Calculate(budget1, icc);
+taxCalculator.Calculate(budget2, icc);
+taxCalculator.Calculate(budget3, icc);
+
+// After executing the program the result must be: 50, 30, 25, 140, 430
