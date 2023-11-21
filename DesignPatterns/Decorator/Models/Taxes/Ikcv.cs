df4473f2a7d2410c8fc9a07472c5645e513aa06d
Julio Barros <juliobarros.dev@gmail.com>
2023-11-21 16:59:59 -0300

    Add one more tax without add any complexicty to other taxes

---
+++ DesignPatterns/Decorator/Models/Taxes/Ikcv.cs
@@ -0,0 +1,16 @@
+﻿using Decorator.Abstractions;
+
+namespace Decorator.Models.Taxes
+{
+    internal class Ikcv : TaxBase
+    {
+        public Ikcv() : base() { }
+
+        public Ikcv(TaxBase anotherTax) : base(anotherTax) { }
+
+        public override double Calculate(Order order)
+        {
+            return order.Value * 0.03 + AnotherTaxValue(order);
+        }
+    }
+}
--- DesignPatterns/Decorator/Program.cs
+++ DesignPatterns/Decorator/Program.cs
@@ -3,15 +3,18 @@ using Decorator.Models.Taxes;
 
 Iss iss = new();
 Iss issAndIcms = new(new Icms());
+Iss issAndIcmsAndIkcv = new(new Icms(new Ikcv()));
 
 Order order = new(500);
 
 var issTax = iss.Calculate(order);
 var issIcmsTax = issAndIcms.Calculate(order);
+var issIcmsIkcvTax = issAndIcmsAndIkcv.Calculate(order);
 
 Console.WriteLine(issTax);
 Console.WriteLine(issIcmsTax);
+Console.WriteLine(issIcmsIkcvTax);
 
 Console.ReadKey();
 
-// After executing the program the result must be: 30 and 80
+// After executing the program the result must be: 30, 80 e 95
