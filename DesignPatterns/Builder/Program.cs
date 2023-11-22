d8fbb92d84c609c55b3810d3ddae8bf8068763ce
Julio Barros <juliobarros.dev@gmail.com>
2023-11-22 17:26:59 -0300

    Add Fluent Interface

--- DesignPatterns/Builder/Program.cs
+++ DesignPatterns/Builder/Program.cs
@@ -3,12 +3,14 @@ using Builder.Utils;
 
 InvoiceBuilder invoiceBuilder = new();
 
-invoiceBuilder.AddCompanyName("Company");
-invoiceBuilder.AddCompanyDocument("Document");
-invoiceBuilder.AddInvoiceItem(new Item("Pen", 10.00));
-invoiceBuilder.AddInvoiceItem(new Item("Pencil", 05.00));
-invoiceBuilder.AddCreatedDate();
-invoiceBuilder.AddComment("Some comment.");
+// Now our builder has a Fluent Interface
+invoiceBuilder
+    .AddCompanyName("Company")
+    .AddCompanyDocument("Document")
+    .AddInvoiceItem(new Item("Pen", 10.00))
+    .AddInvoiceItem(new Item("Pencil", 05.00))
+    .AddCreatedDate()
+    .AddComment("Some comment.");
 
 // Now all the logic to create a new Invoice is inside the InvoiceBuilder and in our Program is easy to read and understand what parameters are needed to create an Invoice.
 Invoice invoice = invoiceBuilder.Build();
--- DesignPatterns/Builder/Utils/InvoiceBuilder.cs
+++ DesignPatterns/Builder/Utils/InvoiceBuilder.cs
@@ -12,16 +12,36 @@ namespace Builder.Utils
         public string Comments { get; private set; } = string.Empty;
         public DateTime CreatedAt { get; private set; }
 
-        public void AddCompanyName(string name) => Name = name;
-        public void AddCompanyDocument(string document) => Document = document;
-        public void AddInvoiceItem(Item item)
+        public InvoiceBuilder AddCompanyName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+        public InvoiceBuilder AddCompanyDocument(string document)
+        {
+            Document = document;
+            return this;
+        }
+
+        public InvoiceBuilder AddInvoiceItem(Item item)
         {
             Items.Add(item);
             Value += item.Value;
             Tax += item.Value * 0.05;
+            return this;
         }
-        public void AddComment(string comment) => Comments = comment;
-        public void AddCreatedDate() => CreatedAt = DateTime.Now;
+
+        public InvoiceBuilder AddComment(string comment) {
+            Comments = comment;
+            return this;
+        }
+
+        public InvoiceBuilder AddCreatedDate()
+        {
+            CreatedAt = DateTime.Now;
+            return this;
+        }
 
         public Invoice Build()
         {
