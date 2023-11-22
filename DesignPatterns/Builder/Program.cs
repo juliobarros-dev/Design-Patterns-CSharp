8b446025b3f64d562e19d56fa18f10ea42a19233
Julio Barros <juliobarros.dev@gmail.com>
2023-11-22 16:48:48 -0300

    Example of the problem that will be solved with Builder Pattern

--- DesignPatterns/Builder/Program.cs
+++ DesignPatterns/Builder/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Builder.Models;
+
+IList<Item> items = new List<Item>();
+
+double invoiceValue = 0;
+
+foreach (var item in items)
+{
+    invoiceValue += item.Value;
+}
+
+double tax = invoiceValue * 0.05;
+
+Invoice invoice = new("Receipt", "Document", DateTime.Now, invoiceValue, tax, items, "Comments");
+
+// Look how complex is to instantiate our Invoice class due to all informations need on its constructor. That's the problem that will be resolved with Builder Patter.
