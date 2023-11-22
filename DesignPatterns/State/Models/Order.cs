1effa8711d170adf62846e3c3fdf5e850c181ccb
Julio Barros <juliobarros.dev@gmail.com>
2023-11-22 14:41:06 -0300

    Initial problem

---
+++ DesignPatterns/State/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace State.Models
+{
+    internal class Order(double value)
+    {
+        public int PENDING { get; set; } = 1;
+        public int APPROVED { get; set; } = 2;
+        public int REFUSED { get; set; } = 3;
+        public int COMPLETED { get; set; } = 4;
+        public int CURRENT_STATE { get; set; }
+        internal double Value { get; private set; } = value;
+
+        public void ApplyDiscount()
+        {
+            // At this point we can already imagine the problem here, whenever we need to add a new state or discount we would need to add more if operators
+            if (CURRENT_STATE == PENDING) Value -= (Value * 0.05);
+            else if (CURRENT_STATE == APPROVED) Value -= (Value * 0.02);
+            else throw new Exception("No more discounts to be applied.");
+        }
+    }
+}
---
+++ DesignPatterns/State/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<p>The State Pattern is a behavioral design pattern that allows an object to alter its behavior when its internal state changes. </p>
+<br>
+<p>As an ilustrative example we will explore discounts based on the order's state</p>
+<br>
+<p><strong>Follow the commit history to understand the problem and see to implementation of the Decorator Pattern </strong></p>
