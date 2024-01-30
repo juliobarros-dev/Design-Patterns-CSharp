82fe2d9dcce71bdae47469ae9df7b184411e09ec
Julio Nascimento <Julio.Nascimento@partner.bmw.com.br>
2024-01-30 12:27:27 -0300

    add inicital setup

---
+++ DesignPatterns/Oberserver/Models/Invoice.cs
@@ -0,0 +1,13 @@
+﻿namespace Observer.Models
+{
+    internal class Invoice(string name, string document, DateTime createdAt, double value, double tax, IList<Item> items, string commments)
+    {
+        public string Name { get; set; } = name;
+        public string Document { get; set; } = document;
+        public DateTime CreatedAt { get; set; } = createdAt;
+        public double Value { get; set; } = value;
+        public double Tax { get; set; } = tax;
+        public IList<Item> Items { get; set; } = items;
+        public string Comments { get; set; } = commments;
+    }
+}
---
+++ DesignPatterns/Oberserver/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace Observer.Models
+{
+    internal class Item(string name, double value)
+    {
+        public string Name { get; private set; } = name;
+        public double Value { get; private set; } = value;
+    }
+}
